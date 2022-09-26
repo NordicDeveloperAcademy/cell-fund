5a1d3f49e4af7c3fffe17ef1faedd2f0c7db5470
Sollie <heidi-irene.sollie@nordicsemi.no>
2022-09-26 12:15:22 +0200

    Added exercise directory to lesson 3, and steps

--- lesson3/cellfund_less3_exer1/src/main.c
+++ lesson3/cellfund_less3_exer1/src/main.c
@@ -13,11 +13,10 @@
 #include <dk_buttons_and_leds.h>
 
 /* STEP 2 - Include the header file for the socket API */
-#include <zephyr/net/socket.h>
+
 
 /* STEP 3 - Define the hostname and port for the echo server */
-#define SERVER_HOSTNAME "nordicecho.westeurope.cloudapp.azure.com"
-#define SERVER_PORT "2444"
+
 
 #define MESSAGE_SIZE 256 
 #define MESSAGE_TO_SEND "Hello from nRF9160 SiP"
@@ -27,8 +26,6 @@ LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
 K_SEM_DEFINE(lte_connected, 0, 1);
 
 /* STEP 4 - Declare the structure for the socket and server address */
-static int sock;
-static struct sockaddr_storage server;
 
 static uint8_t recv_buf[MESSAGE_SIZE];
 
@@ -36,39 +33,12 @@ static uint8_t recv_buf[MESSAGE_SIZE];
 static int server_resolve(void)
 {
 	/* STEP 6.1 - Call getaddrinfo() to get the IP address of the echo server */
-	int err;
-	struct addrinfo *result;
-	struct addrinfo hints = {
-		.ai_family = AF_INET,
-		.ai_socktype = SOCK_DGRAM
-	};
-	
-	err = getaddrinfo(SERVER_HOSTNAME, SERVER_PORT, &hints, &result);
-	if (err != 0) {
-		LOG_INF("ERROR: getaddrinfo failed %d", err);
-		return -EIO;
-	}
-
-	if (result == NULL) {
-		LOG_INF("ERROR: Address not found");
-		return -ENOENT;
-	} 	
 
 	/* STEP 6.2 - Retrieve the relevant information from the result structure*/
-	struct sockaddr_in *server4 = ((struct sockaddr_in *)&server);
-	server4->sin_addr.s_addr =
-		((struct sockaddr_in *)result->ai_addr)->sin_addr.s_addr;
-	server4->sin_family = AF_INET;
-	server4->sin_port = ((struct sockaddr_in *)result->ai_addr)->sin_port;
 	
 	/* STEP 6.3 - Convert the address into a string and print it */
-	char ipv4_addr[NET_IPV4_ADDR_LEN];
-	inet_ntop(AF_INET, &server4->sin_addr.s_addr, ipv4_addr,
-		  sizeof(ipv4_addr));
-	LOG_INF("IPv4 Address found %s", ipv4_addr);
 	
 	/* STEP 6.4 - Free the memory allocated for result */
-	freeaddrinfo(result);
 
 	return 0;
 }
@@ -95,19 +65,8 @@ static int udp_socket_connect(void)
 {
 	int err;
 	/* STEP 7 - Create a UDP socket */
-	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock < 0) {
-		LOG_ERR("Failed to create socket: %d.", errno);
-		return -errno;
-	}
 
 	/* STEP 8 - Connect the socket to the server */
-	err = connect(sock, (struct sockaddr *)&server,
-		      sizeof(struct sockaddr_in));
-	if (err < 0) {
-		LOG_ERR("Connect failed : %d", errno);
-		return -errno;
-	}
 
 	return 0;
 }
@@ -117,12 +76,7 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 	switch (has_changed) {
 	case DK_BTN1_MSK:
 		/* STEP 9 - call send() when button 1 is pressed */
-		if (button_states & DK_BTN1_MSK){	
-			int err = send(sock, MESSAGE_TO_SEND, SSTRLEN(MESSAGE_TO_SEND), 0);
-			if (err < 0) {
-				LOG_INF("Failed to send message, %d", errno);
-				return;	
-			}
+
 		}
 		break;
 
@@ -165,19 +119,6 @@ void main(void)
 	LOG_INF("Press Button 1 on your Development Kit or Thingy:91 to send your message ");
 		while (1) {
 			/* STEP 10 - Call recv() to listen to received messages */
-			int len = recv(sock, recv_buf, sizeof(recv_buf) - 1, 0);
-
-			if (len < 0) {
-				LOG_ERR("Error reading response\n");
-				break;
-			}
-
-			if (len == 0) {
-				break;
-			}
-
-			recv_buf[len] = 0;
-			LOG_INF("Data received from the server: (%s)", recv_buf);
 			
 		}
 		(void)close(sock);
