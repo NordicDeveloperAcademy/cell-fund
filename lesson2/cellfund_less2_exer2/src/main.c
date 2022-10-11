68ac497fb21a87e275440df7d9301ed2e59d099e
Sollie <heidi-irene.sollie@nordicsemi.no>
2022-10-11 15:32:55 +0200

    Consistent header files

--- lesson2/cellfund_less2_exer2/src/main.c
+++ lesson2/cellfund_less2_exer2/src/main.c
@@ -5,32 +5,57 @@
  */
 
 #include <stdio.h>
-#include <dk_buttons_and_leds.h>
+#include <zephyr/kernel.h>
 #include <logging/log.h>
-/* STEP 4 - Include the header file of the LTE link controller library */
+#include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
-#define REGISTERED_STATUS_LED          DK_LED2
+/* STEP 4 - Include the header file of the LTE link controller library */
 
 /* STEP 5 - Define the semaphore lte_connected */
 
-/* STEP 7 - Define the callback function lte_handler() */ 
+LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
+
+/* STEP 7 - Define the event handler for LTE link control */ 
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+	switch (evt->type) {
+	/* STEP 7.1 - Define the callback function lte_handler()*/ 
+	case LTE_LC_EVT_NW_REG_STATUS:
+		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+			(evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+			break;
+		}
+		LOG_INF("Network registration status: %s",
+				evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+				"Connected - home network" : "Connected - roaming");
+		k_sem_give(&lte_connected);
+		break;
+	/* STEP 7.2 - On event RRC update, print RRC mode */
+	case LTE_LC_EVT_RRC_UPDATE:
+		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ? 
+				"Connected" : "Idle");
+		break;			
+	default:
+		break;
+	}
+} 
 
 /* STEP 6 - Define the function modem_configure() to initialize an LTE connection */
 
 void main(void)
 {
-	int ret = dk_leds_init();
-	if (ret){
-		LOG_ERR("Failed to initlize the LEDs Library");
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LEDs Library");
 	}
+
 	/* STEP 8 - Call modem_configure() to initiate the LTE connection */
 
-	LOG_INF("Connecting to LTE network, this may take several minutes...");
+	LOG_INF("Connecting to LTE network");
 
 	/* STEP 9 - Take the semaphore lte_connected */
 
-	//We only reach here if we are registered to a network
 	LOG_INF("Connected to LTE network");
-	dk_set_led_on(REGISTERED_STATUS_LED);
+
+	/* STEP 10 - Turn on the LED status LED */
+	dk_set_led_on(DK_LED2);
 }
--- lesson2/cellfund_less2_exer2_solution/src/main.c
+++ lesson2/cellfund_less2_exer2_solution/src/main.c
@@ -5,21 +5,23 @@
  */
 
 #include <stdio.h>
-#include <dk_buttons_and_leds.h>
+#include <zephyr/kernel.h>
 #include <logging/log.h>
+#include <dk_buttons_and_leds.h>
+
 /* STEP 4 - Include the header file of the LTE link controller library */
 #include <modem/lte_lc.h>
-
-LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
-#define REGISTERED_STATUS_LED          DK_LED2
 
 /* STEP 5 - Define the semaphore lte_connected */
 static K_SEM_DEFINE(lte_connected, 0, 1);
 
-/* STEP 7 - Define the callback function lte_handler()*/ 
+LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
+
+/* STEP 7 - Define the event handler for LTE link control */ 
 static void lte_handler(const struct lte_lc_evt *const evt)
 {
 	switch (evt->type) {
+	/* STEP 7.1 - Define the callback function lte_handler()*/ 
 	case LTE_LC_EVT_NW_REG_STATUS:
 		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
 			(evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
@@ -30,6 +32,11 @@ static void lte_handler(const struct lte_lc_evt *const evt)
 				"Connected - home network" : "Connected - roaming");
 		k_sem_give(&lte_connected);
 		break;
+	
+	case LTE_LC_EVT_RRC_UPDATE:
+		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ? 
+				"Connected" : "Idle");
+		break;			
 	default:
 		break;
 	}
@@ -47,19 +54,20 @@ static void modem_configure(void)
 
 void main(void)
 {
-	int ret = dk_leds_init();
-	if (ret){
-		LOG_ERR("Failed to initlize the LEDs Library");
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LEDs Library");
 	}
+
 	/* STEP 8 - Call modem_configure() to initiate the LTE connection */
 	modem_configure();
 
-	LOG_INF("Connecting to LTE network, this may take several minutes...");
+	LOG_INF("Connecting to LTE network");
 
 	/* STEP 9 - Take the semaphore lte_connected */
 	k_sem_take(&lte_connected, K_FOREVER);
 	
-	//We only reach here if we are registered to a network
 	LOG_INF("Connected to LTE network");
-	dk_set_led_on(REGISTERED_STATUS_LED);
+
+	/* STEP 10 - Turn on the LED status LED */
+	dk_set_led_on(DK_LED2);
 }
