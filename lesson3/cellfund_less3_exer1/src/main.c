27425b44df5958976a366c3fd6edc8110cf63b1c
Sollie <heidi-irene.sollie@nordicsemi.no>
2022-10-11 16:09:54 +0200

    Changed order of header files and functions

--- lesson3/cellfund_less3_exer1/src/main.c
+++ lesson3/cellfund_less3_exer1/src/main.c
@@ -5,31 +5,30 @@
  */
 
 #include <stdio.h>
-#include <string.h>
-
 #include <zephyr/kernel.h>
-#include <modem/lte_lc.h>
 #include <logging/log.h>
 #include <dk_buttons_and_leds.h>
+#include <modem/lte_lc.h>
 
-/* STEP 2 - Include the header file for the socket API */
+/* STEP 3 - Include the header file for the socket API */
 
 
-/* STEP 3 - Define the hostname and port for the echo server */
+/* STEP 4 - Define the hostname and port for the echo server */
 
 
 #define MESSAGE_SIZE 256 
 #define MESSAGE_TO_SEND "Hello from nRF9160 SiP"
 #define SSTRLEN(s) (sizeof(s) - 1)
 
-LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
+/* STEP 5.1 - Declare the structure for the socket and server address */
+
+
+/* STEP 5.2 - Declare the buffer for receiving from server */
+
 K_SEM_DEFINE(lte_connected, 0, 1);
 
-/* STEP 4 - Declare the structure for the socket and server address */
+LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
 
-static uint8_t recv_buf[MESSAGE_SIZE];
-
-/**@brief Resolves the configured hostname. */
 static int server_resolve(void)
 {
 	/* STEP 6.1 - Call getaddrinfo() to get the IP address of the echo server */
@@ -43,32 +42,54 @@ static int server_resolve(void)
 	return 0;
 }
 
-static void lte_handler(const struct lte_lc_evt *const evt)
-{
-     switch (evt->type) {
-     case LTE_LC_EVT_NW_REG_STATUS:
-             if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
-             (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
-                     break;
-             }
-
-             LOG_INF("Connected to: %s network",
-             evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ? "home" : "roaming");
-			 k_sem_give(&lte_connected);
-             break;
-     default:
-             break;
-     }
-}
-
-static int udp_socket_connect(void)
+static int server_connect(void)
 {
 	int err;
 	/* STEP 7 - Create a UDP socket */
 
 	/* STEP 8 - Connect the socket to the server */
 
+	LOG_INF("Successfully connected to server");
+
 	return 0;
+}
+
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+     switch (evt->type) {
+     case LTE_LC_EVT_NW_REG_STATUS:
+        if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+            (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+            break;
+        }
+		LOG_INF("Network registration status: %s",
+				evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+				"Connected - home network" : "Connected - roaming");
+		k_sem_give(&lte_connected);
+        break;
+	case LTE_LC_EVT_RRC_UPDATE:
+		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ? 
+				"Connected" : "Idle");
+		break;				 
+     default:
+             break;
+     }
+}
+
+static void modem_configure(void)
+{
+	int err;
+	
+	LOG_INF("Connecting to LTE network"); 
+	
+	err = lte_lc_init_and_connect_async(lte_handler);
+	if (err) {
+		LOG_INF("Modem could not be configured, error: %d", err);
+		return;
+	}
+	k_sem_take(&lte_connected, K_FOREVER);
+	LOG_INF("Connected to LTE network");
+	dk_set_led_on(DK_LED2);
 }
 
 static void button_handler(uint32_t button_states, uint32_t has_changed)
@@ -83,43 +104,35 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 	}
 }
 
-static void modem_configure(void)
-{
-	int err = lte_lc_init_and_connect_async(lte_handler);
-	if (err) {
-		LOG_INF("Modem could not be configured, error: %d", err);
-		return;
-	}
-}
-
 void main(void)
 {
-	int err;
+	int err, received;
+
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LED library");
+	}
+
 	modem_configure();
-	LOG_INF("Connecting to LTE network, this may take several minutes...");
-	k_sem_take(&lte_connected, K_FOREVER);	
-	err = dk_buttons_init(button_handler);
-	if (err){
-		LOG_ERR("Failed to initlize the Buttons Library");
+
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
 	}
-	err = dk_leds_init();
-	if (err){
-		LOG_ERR("Failed to initlize the LEDs Library");
-	}
-	dk_set_led_on(DK_LED2);	
+
 	if (server_resolve() != 0) {
 		LOG_INF("Failed to resolve server name");
 		return;
 	}
 	
-	if (udp_socket_connect() != 0) {
+	if (server_connect() != 0) {
 		LOG_INF("Failed to initialize client");
 		return;
 	}
-	LOG_INF("Press Button 1 on your Development Kit or Thingy:91 to send your message ");
+
+	LOG_INF("Press button 1 on your Development Kit or Thingy:91 to send your message");
+
 		while (1) {
 			/* STEP 10 - Call recv() to listen to received messages */
-			
+
 		}
 		(void)close(sock);
 }
--- lesson3/cellfund_less3_exer1_solution/src/main.c
+++ lesson3/cellfund_less3_exer1_solution/src/main.c
@@ -5,17 +5,15 @@
  */
 
 #include <stdio.h>
-#include <string.h>
-
 #include <zephyr/kernel.h>
-#include <modem/lte_lc.h>
 #include <logging/log.h>
 #include <dk_buttons_and_leds.h>
+#include <modem/lte_lc.h>
 
-/* STEP 2 - Include the header file for the socket API */
+/* STEP 3 - Include the header file for the socket API */
 #include <zephyr/net/socket.h>
 
-/* STEP 3 - Define the hostname and port for the echo server */
+/* STEP 4 - Define the hostname and port for the echo server */
 #define SERVER_HOSTNAME "nordicecho.westeurope.cloudapp.azure.com"
 #define SERVER_PORT "2444"
 
@@ -23,16 +21,17 @@
 #define MESSAGE_TO_SEND "Hello from nRF9160 SiP"
 #define SSTRLEN(s) (sizeof(s) - 1)
 
-LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
-K_SEM_DEFINE(lte_connected, 0, 1);
-
-/* STEP 4 - Declare the structure for the socket and server address */
+/* STEP 5.1 - Declare the structure for the socket and server address */
 static int sock;
 static struct sockaddr_storage server;
 
+/* STEP 5.2 - Declare the buffer for receiving from server */
 static uint8_t recv_buf[MESSAGE_SIZE];
 
-/**@brief Resolves the configured hostname. */
+K_SEM_DEFINE(lte_connected, 0, 1);
+
+LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
+
 static int server_resolve(void)
 {
 	/* STEP 6.1 - Call getaddrinfo() to get the IP address of the echo server */
@@ -73,25 +72,7 @@ static int server_resolve(void)
 	return 0;
 }
 
-static void lte_handler(const struct lte_lc_evt *const evt)
-{
-     switch (evt->type) {
-     case LTE_LC_EVT_NW_REG_STATUS:
-             if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
-             (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
-                     break;
-             }
-
-             LOG_INF("Connected to: %s network",
-             evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ? "home" : "roaming");
-			 k_sem_give(&lte_connected);
-             break;
-     default:
-             break;
-     }
-}
-
-static int udp_socket_connect(void)
+static int server_connect(void)
 {
 	int err;
 	/* STEP 7 - Create a UDP socket */
@@ -108,16 +89,55 @@ static int udp_socket_connect(void)
 		LOG_ERR("Connect failed : %d", errno);
 		return -errno;
 	}
+	LOG_INF("Successfully connected to server");
 
 	return 0;
 }
 
-static void button_handler(uint32_t button_states, uint32_t has_changed)
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+     switch (evt->type) {
+     case LTE_LC_EVT_NW_REG_STATUS:
+        if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+            (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+            break;
+        }
+		LOG_INF("Network registration status: %s",
+				evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+				"Connected - home network" : "Connected - roaming");
+		k_sem_give(&lte_connected);
+        break;
+	case LTE_LC_EVT_RRC_UPDATE:
+		LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ? 
+				"Connected" : "Idle");
+		break;				 
+     default:
+             break;
+     }
+}
+
+static void modem_configure(void)
+{
+	int err;
+	
+	LOG_INF("Connecting to LTE network"); 
+	
+	err = lte_lc_init_and_connect_async(lte_handler);
+	if (err) {
+		LOG_INF("Modem could not be configured, error: %d", err);
+		return;
+	}
+	k_sem_take(&lte_connected, K_FOREVER);
+	LOG_INF("Connected to LTE network");
+	dk_set_led_on(DK_LED2);
+}
+
+static void button_handler(uint32_t button_state, uint32_t has_changed)
 {
 	switch (has_changed) {
 	case DK_BTN1_MSK:
 		/* STEP 9 - call send() when button 1 is pressed */
-		if (button_states & DK_BTN1_MSK){	
+		if (button_state & DK_BTN1_MSK){	
 			int err = send(sock, MESSAGE_TO_SEND, SSTRLEN(MESSAGE_TO_SEND), 0);
 			if (err < 0) {
 				LOG_INF("Failed to send message, %d", errno);
@@ -125,58 +145,49 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 			}
 		}
 		break;
-
-	}
-}
-
-static void modem_configure(void)
-{
-	int err = lte_lc_init_and_connect_async(lte_handler);
-	if (err) {
-		LOG_INF("Modem could not be configured, error: %d", err);
-		return;
 	}
 }
 
 void main(void)
 {
-	int err;
+	int err, received;
+
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LED library");
+	}
+
 	modem_configure();
-	LOG_INF("Connecting to LTE network, this may take several minutes...");
-	k_sem_take(&lte_connected, K_FOREVER);	
-	err = dk_buttons_init(button_handler);
-	if (err){
-		LOG_ERR("Failed to initlize the Buttons Library");
+
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
 	}
-	err = dk_leds_init();
-	if (err){
-		LOG_ERR("Failed to initlize the LEDs Library");
-	}
-	dk_set_led_on(DK_LED2);	
+
 	if (server_resolve() != 0) {
 		LOG_INF("Failed to resolve server name");
 		return;
 	}
 	
-	if (udp_socket_connect() != 0) {
+	if (server_connect() != 0) {
 		LOG_INF("Failed to initialize client");
 		return;
 	}
-	LOG_INF("Press Button 1 on your Development Kit or Thingy:91 to send your message ");
+
+	LOG_INF("Press button 1 on your Development Kit or Thingy:91 to send your message");
+
 		while (1) {
 			/* STEP 10 - Call recv() to listen to received messages */
-			int len = recv(sock, recv_buf, sizeof(recv_buf) - 1, 0);
+			received = recv(sock, recv_buf, sizeof(recv_buf) - 1, 0);
 
-			if (len < 0) {
-				LOG_ERR("Error reading response\n");
+			if (received < 0) {
+				LOG_ERR("Socket error: %d, exit", errno);
 				break;
 			}
 
-			if (len == 0) {
+			if (received == 0) {
 				break;
 			}
 
-			recv_buf[len] = 0;
+			recv_buf[received] = 0;
 			LOG_INF("Data received from the server: (%s)", recv_buf);
 			
 		}
